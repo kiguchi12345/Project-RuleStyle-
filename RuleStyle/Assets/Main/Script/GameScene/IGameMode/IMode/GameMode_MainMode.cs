7470932b1f7da1c6bfe345d3c942a166dad404d2
23A31A0050 <23A31A0050@edu.kyoto-tech.ac.jp>
2025-01-31 16:50:53 +0900

    ショット関数

--- RuleStyle/Assets/Main/Script/GameScene/IGameMode/IMode/GameMode_MainMode.cs
+++ RuleStyle/Assets/Main/Script/GameScene/IGameMode/IMode/GameMode_MainMode.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class GameMode_MainMode : IGameMode
 {
@@ -9,13 +10,20 @@ public class GameMode_MainMode : IGameMode
 
     PlayerSessionData player;
 
-    //カメラの位置
-    //public Transform Camera;
+    #region カメラコンポーネント
     /// <summary>
     /// 中心
     /// </summary>
     public Vector3 Cont = new Vector3(0, 0, 0);
-    public int Dist = 10;
+    public float Dist = 3f;
+    #endregion
+
+    public ReactiveProperty<bool> dragged = new ReactiveProperty<bool>(false);
+
+    private Vector3 position;
+    private Rigidbody rb;
+    private Vector3 dragOffset;
+
     public GameMode_MainMode(GameSessionManager gameSceneManager)
     {
         GameSceneManager = gameSceneManager;
@@ -24,7 +32,7 @@ public class GameMode_MainMode : IGameMode
     /// <summary>
     /// 変更する数値
     /// </summary>
-    public int ChangeMeter = 0;
+    public float ChangeMeter = 0;
 
     /// <summary>
     /// ここで次のプレイヤーの情報を入れる
@@ -41,6 +49,13 @@ public class GameMode_MainMode : IGameMode
 
         //現在のキャラクターの地点
         Cont = player.Player_GamePiece.transform.position;
+        Cont.y = 0;
+
+
+        rb=player.Player_GamePiece.GetComponent<Rigidbody>();
+        dragged.Where(_ => _ == true).Subscribe(_ => {
+            position = Input.mousePosition;
+        }).AddTo(player.Player_GamePiece);
     }
 
     /// <summary>
@@ -49,14 +64,13 @@ public class GameMode_MainMode : IGameMode
     void IGameMode.Update()
     {
         CameraRole();
+
+        Line();
     }
 
-    /// <summary>
-    /// カメラ制御とカメラの回転
-    /// </summary>
     void CameraRole()
     {
-        //上限値に行けば数値を戻す
+        // 上限値に行けば数値を戻す
         if (ChangeMeter >= 361)
         {
             ChangeMeter = 0;
@@ -65,39 +79,86 @@ public class GameMode_MainMode : IGameMode
         {
             ChangeMeter = 360;
         }
-        //数値を変更
-        if (Input.GetMouseButton(1))
-        {
-            ChangeMeter++;
-        }
+
+        // 数値を変更
         if (Input.GetMouseButton(2))
         {
-            ChangeMeter--;
+            ChangeMeter += 0.3f;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            ChangeMeter -= 0.3f;
         }
 
-        //ラジアン変換
+        //現在のキャラクターの地点
+        Cont = player.Player_GamePiece.transform.position;
+        Cont.y = 0;
+
+        // ラジアン変換
         float test = ChangeMeter * Mathf.Deg2Rad;
-        //移動するべき地点を算出
+        // 移動するべき地点を算出
         float b = Mathf.Cos(test);
         float a = Mathf.Sin(test);
-        //地点作成
-        Vector3 t = new Vector3(b * Dist, 10, a * Dist);
-        //カメラの位置を変更
-        GameSceneManager.CameraPosition.transform.position = t;
+        // 地点作成
+        Vector3 t = new Vector3(b * Dist, 3f, a * Dist);
+        // カメラの位置を変更
+        GameSceneManager.CameraPosition.transform.position = t + Cont;
 
-        Vector3 direction = Cont - t;
-        
-        // カメラの回転を計算
-        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        // プレイヤーの方向を向かせる
+        GameSceneManager.CameraPosition.transform.LookAt(player.Player_GamePiece.transform);
 
-        // X 軸の回転を強制的に設定
-        Vector3 eulerRotation = targetRotation.eulerAngles;
-        eulerRotation.x = 30f; // 任意の角度に固定、例えば30度
-        targetRotation = Quaternion.Euler(eulerRotation);
+        // 特定のX軸角度を設定（例えば30度）
+        float fixedXRotation = 30f;
 
-        // 回転を適用
-        GameSceneManager.CameraPosition.rotation = targetRotation;
+        // 現在の回転を取得
+        Vector3 fixedRotation = GameSceneManager.CameraPosition.transform.rotation.eulerAngles;
 
+        // X軸回転を固定（特定の値に設定）
+        fixedRotation.x = fixedXRotation;
+
+        // Y軸とZ軸の回転を維持しつつ、X軸だけ更新
+        GameSceneManager.CameraPosition.transform.rotation = Quaternion.Euler(fixedRotation);
+    }
+
+    public void Line()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            dragged.Value = true;
+
+            //ドラッグ開始からの差値のベクトル
+            Vector3 direction = Input.mousePosition - position;
+            
+
+            //ラジアン角度抽出
+            var rad = Mathf.Atan2(direction.y, direction.x);
+            Debug.Log(rad);
+
+            float x = Mathf.Cos(rad);
+            float z = Mathf.Sin(rad);
+
+            //ベクトル作成
+            dragOffset = new Vector3(x, 0, z);
+
+            //長さが10以降だった場合
+            if (direction.magnitude > 10)
+            {
+                Debug.DrawLine(player.Player_GamePiece.transform.position, new Vector3(dragOffset.x * 10, 1, dragOffset.z * 10), Color.black);
+            }
+            //長さが10以前だった場合
+            else
+            {
+                Debug.DrawLine(player.Player_GamePiece.transform.position, new Vector3(dragOffset.x * direction.magnitude, 1, dragOffset.z * direction.magnitude), Color.black);
+            }
+
+        }
+        //離した時
+        else if (dragged.Value == true)
+        {
+            dragged.Value = false;
+
+            rb.AddForce(dragOffset * 30, ForceMode.Impulse);
+        }
     }
 
     void IGameMode.FixUpdate()
